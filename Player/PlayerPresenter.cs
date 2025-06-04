86cdb809916e066d81c6ccd2d8a8aea0238a76ee
Filosoforaptor <166240855+Filosoforaptor@users.noreply.github.com>
2025-06-04 20:42:48 -0300

    Add files via upload
    ultimo push limpiando

--- Player/PlayerPresenter.cs
+++ Player/PlayerPresenter.cs
@@ -12,8 +12,6 @@ public class PlayerPresenter : MonoBehaviour
     private GameObject _mesh;
     public Action<bool> OnPlayerMoving { get; set; }
     public Action<int> OnCoinsCollected { get; set; }
-    public Action<int, int> OnPlayerHealthChangedUI { get; set; } // For the view
-    public Action OnPlayerDiedUI { get; set; }
 
     void Awake()
     {
@@ -25,25 +23,13 @@ public class PlayerPresenter : MonoBehaviour
     private void OnEnable()
     {
         _model.OnCoinsChanged += PresenterCoinsChanged;
-        // Subscribe to health events from the model
-        if (_model != null) // Good practice to check if model exists
-        {
-            _model.OnHealthChanged += PresenterHealthChanged;
-            _model.OnPlayerDied += PresenterPlayerDied;
-        }
     }
 
     private void OnDisable()
     {
         _model.OnCoinsChanged -= PresenterCoinsChanged;
-        // Unsubscribe from health events
-        if (_model != null) // Good practice to check if model exists
-        {
-            _model.OnHealthChanged -= PresenterHealthChanged;
-            _model.OnPlayerDied -= PresenterPlayerDied;
-        }
-    }
 
+    }
     void FixedUpdate()
     {
         Vector3 input = _pInput.Axis;
@@ -66,52 +52,17 @@ public class PlayerPresenter : MonoBehaviour
         Quaternion currentRotation = _mesh.transform.localRotation;
         _mesh.transform.localRotation = Quaternion.Slerp(currentRotation, targetRotation, _model.TiltSpeed * Time.fixedDeltaTime);
     }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
         {
             _model.AddCoin();
             Destroy(other.gameObject);
+   
         }
-        // Note: Meteorite collision is handled by Meteorite.cs calling ProcessDamage on this presenter.
     }
-
     private void PresenterCoinsChanged(int newCoinCount)
     {
         OnCoinsCollected?.Invoke(newCoinCount);
-    }
-
-    // Method called by Meteorite to inflict damage
-    public void ProcessDamage(int damageAmount)
-    {
-        if (_model != null)
-        {
-            _model.TakeDamage(damageAmount);
-        }
-    }
-
-    // Handler for the model's OnHealthChanged event
-    private void PresenterHealthChanged(int currentHealth, int maxHealth)
-    {
-        OnPlayerHealthChangedUI?.Invoke(currentHealth, maxHealth);
-    }
-
-    // Handler for the model's OnPlayerDied event
-    private void PresenterPlayerDied()
-    {
-        Debug.Log("Player has died! (Presenter)");
-        OnPlayerDiedUI?.Invoke();
-        if (_pInput != null)
-        {
-            _pInput.enabled = false; // Disable the PlayerInput component
-        }
-        if (_rb != null)
-        {
-            _rb.velocity = Vector3.zero;
-            _rb.isKinematic = true; // Makes it ignore physics forces, good for a "dead" state
-        }
-        // Consider disabling this component or the entire GameObject if appropriate
-        // this.enabled = false; 
     }
 }
